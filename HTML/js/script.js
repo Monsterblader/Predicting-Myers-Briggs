67d1f61a643f4c464882786fe4c2bff2812e888a
Monsterblader <Monsterblader@gmail.com>
2020-10-27 01:33:27 -0700

    Saves progress on slideshow.

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,40 +1,40 @@
-const page = {
-    min: 1,
-    max: 22,
-    no: 1,
-};
-
-const makePageNumber = num => {
-    return num < 10 ? "page0" + num : "page" + num;
-}
-
-const nextPage = e => {
-    e && e.preventDefault();
-    if (page.no < page.max) {
-        $('.' + makePageNumber(page.no)).removeClass('active');
-        page.no += 1;
-        $('.' + makePageNumber(page.no)).addClass('active');
-    }
-}
-
-const prevPage = e => {
-    e && e.preventDefault();
-    if (page.no > page.min) {
-        $('.' + makePageNumber(page.no)).removeClass('active');
-        page.no -= 1;
-        $('.' + makePageNumber(page.no)).addClass('active');
-    }
-}
-
-document.onkeydown = checkKey;
-
-function checkKey(e) {
+const checkKey = e => {
     const err = e || window.event;
 
     if (e.keyCode == '37') {
-        prevPage();
+        plusSlides(-1);
     }
     else if (e.keyCode == '39') {
-        nextPage();
+        plusSlides(1);
     }
 }
+
+// Next/previous controls
+const plusSlides = n => {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+const currentSlide = n => {
+  showSlides(slideIndex = n);
+}
+
+const showSlides = n => {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+document.onkeydown = checkKey;
